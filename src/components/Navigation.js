ded453d8083474795d2e94f0a6bea609b09283f0
alichaabane <alichaabane98@gmail.com>
2022-01-05 00:07:31 +0100

    add scroll To and scroll Up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,15 +3,17 @@ import content from '../content';
 export default function Navigation() {
   return (
     <div style={{ background: "#091c29" }}>
-      <div
-        className="flex items-center justify-between w-10/12 mx-auto text-white  py-3"
-      >
+      <div className="flex items-center justify-between w-10/12 mx-auto text-white  py-3">
         <h1 className="text-3xl font-desis animate-pulse  font-bold">{content.nav.logo} {' '}
         <span className="w-3 h-3 bg-red-500 inline-block rounded-full"> </span>
         </h1>
         <div>
           {content.nav.links.map((link, index) => {
-            return <span className="text-xl mr-4">{link.text}</span>;
+            return (
+                <span key={'element-' + index} className="text-xl mr-4">
+                {link.text}
+                </span>
+              );
           })}
         </div>
       </div>
